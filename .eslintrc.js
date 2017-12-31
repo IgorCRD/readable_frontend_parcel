c7e93ca4752cc3f4155607a9b0d1a68ee5c02bf1
Igor Crispim Diniz <igor.crispimdiniz@gmail.com>
2017-12-31 08:59:50 -0300

    Adding environment info to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "indent": ["error", 4]
+    },
+    "env": {
+        "browser": true,
+        "node": true
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
 if (module.hot) {
-    module.hot.dispose(function () {
-      // module is about to be replaced
+    module.hot.dispose(() => {
+        // module is about to be replaced
     });
-    module.hot.accept(function () {
-      // module or one of its dependencies was just updated
+    module.hot.accept(() => {
+        // module or one of its dependencies was just updated
     });
 }
