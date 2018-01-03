4d7422c279982a4b1eb5889f16a76381541d4863
Igor Crispim Diniz <igor.crispimdiniz@gmail.com>
2018-01-03 14:22:29 -0300

    configuring eslint to work better with react and styled components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,30 @@
 module.exports = {
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true,
+            "experimentalObjectRestSpread": true
+        }
+    },
     "extends": "airbnb",
     "rules": {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "indent": ["error", 4]
+        "indent": ["error", 4],
+        "react/jsx-indent": [4, 4],
+        "react/jsx-indent-props": [4, 4],
+        "react/no-did-mount-set-state": [false]
     },
     "env": {
         "browser": true,
         "node": true
+    },
+    "settings": {
+        "react": {
+            "pragma": "React",
+            "version": "16.2.0"
+        }
     }
 };
