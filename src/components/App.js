442ffb4835c16a84574ce7fae15a67ac094bc737
Igor Crispim Diniz <igor.crispimdiniz@gmail.com>
2018-01-04 23:03:48 -0300

    added react-bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import Navbar from './Navbar';
 import Lorem from './Lorem';
 import Container from './Container';
 
 const App = () => (
     <div>
-        <Navbar>
-            <h1>Teste</h1>
+        <Navbar fixedTop padd>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#brand">Readable</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <NavItem eventKey={1} href="#">
+                        Link
+                    </NavItem>
+                    <NavItem eventKey={2} href="#">
+                        Link
+                    </NavItem>
+                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}>Action</MenuItem>
+                        <MenuItem eventKey={3.2}>Another action</MenuItem>
+                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                    </NavDropdown>
+                </Nav>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="#">
+                        Link Right
+                    </NavItem>
+                    <NavItem eventKey={2} href="#">
+                        Link Right
+                    </NavItem>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
         <Container>
             <Lorem Title="Lorem ipsum" />
+            <Lorem />
+            <Lorem />
+            <Lorem />
+            <Lorem />
+            <Lorem />
             <Lorem />
             <Lorem />
             <Lorem />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Container from './Container';
+import { Navbar } from 'react-bootstrap';
+
+const CustomNavbar = styled(Navbar)`
+    border-radius: 0 !important;
+    margin-bottom: 0;
+    padding-top: 15px;
+    border-bottom: 1px solid transparent;
+    border: 0;
+    font-size: 16px;
+    color: #FFFFFF !important;
+    opacity: 0.9;
+    filter: alpha(opacity=90);
+    transition: all 0.4s;
+    -webkit-transition: all 0.4s;
+
+    padding-top: ${props => (props.transparent ? '15px' : '0px')};
+    border-bottom: ${props => (props.transparent ? '1px solid transparent' : '')};
+    background-color: ${props => (props.transparent ? 'transparent' : 'rgba(52, 172, 220, 0.98)')};
+
+    @media (max-width: 767px){
+        background-color: ${props => (props.transparent ? 'rgba(0, 0, 0, 0.98)' : 'rgba(52, 172, 220, 0.98)')}; 
+    }
+
+    li * {
+        background-color: transparent;
+        border-radius: 3px;
+        color: #FFFFFF !important;
+        opacity: 1;
+        filter: alpha(opacity=100);
+    }
+`;
+CustomNavbar.propTypes = { transparent: PropTypes.bool };
 
 const NavbarPlaceHolder = styled.div`
     padding-bottom: ${props => `${props.height}px`};
+    background-color: black;
 `;
 
-const NavbarStyled = styled.nav`
-    padding-top: ${props => (props.transparent ? '15px' : '0px')};
-    border-bottom: ${props => (props.transparent ? '1px solid transparent' : '')};
-    border-radius: 0 !important;
-    border-width: 0 0 1px;
-    border: 0;
-    margin-bottom: 0;
-    top: 0;
-    right: 0;
-    left: 0;
-    z-index: 1030;
-    position: fixed;
-    min-height: 50px;
-
-    background-color: ${props => (props.transparent ? 'transparent' : 'rgba(52, 172, 220, 0.98)')};
-    
-    transition: all 0.4s;
-    -webkit-transition: all 0.4s;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    
-    -webkit-transition: all 300ms linear;
-    -moz-transition: all 300ms linear;
-    -o-transition: all 300ms linear;
-    -ms-transition: all 300ms linear;
-    transition: all 300ms linear; 
-
-    font-family: "Helvetica Neue","Open Sans",Arial,sans-serif;
-    font-size: 16px;
-
-    :before {
-        display: table;
-        content: " ";
-    }
-
-    :after {
-        display: table;
-        content: " ";
-        clear: both;
-    }
-    
-    @media (max-width: 767px){
-        padding-top: 0px;
-        background-color: ${props => (props.transparent ? 'rgba(0, 0, 0, 0.45)' : 'rgba(52, 172, 220, 0.98)')}; 
-    }
-
-    @media (min-width: 768px) {
-        border-radius: 4px;
-    }
-`;
-
-class Navbar extends React.Component {
+class NavbarTransparent extends Navbar {
     static propTypes = {
-        children: PropTypes.object,
+        children: PropTypes.arrayOf(PropTypes.object),
     }
 
     static defaultProps = {
@@ -74,8 +55,13 @@ class Navbar extends React.Component {
 
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll);
+        window.addEventListener('resize', () => {
+            this.setState({
+                height: this.nav.nextElementSibling.clientHeight,
+            });
+        });
         this.setState({
-            height: this.nav.clientHeight,
+            height: this.nav.nextElementSibling.clientHeight,
         });
     }
 
@@ -91,19 +77,49 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <Container fullWidth>
-                <NavbarPlaceHolder height={this.state.height} />
-                <NavbarStyled
-                    transparent={this.state.scrollTop <= 260}
+            <div>
+                <NavbarPlaceHolder
+                    height={this.state.height}
                     innerRef={(ref) => { this.nav = ref; }}
+                />
+                <CustomNavbar
+                    fixedTop
+                    transparent={this.state.scrollTop <= this.state.height / 2}
                 >
-                    <Container>
-                        { this.props.children }
-                    </Container>
-                </NavbarStyled>
-            </Container>
+                    { this.props.children }
+                </CustomNavbar>
+            </div>
         );
     }
 }
 
-export default Navbar;
+NavbarTransparent.Brand = styled(Navbar.Brand)`
+    color: #FFFFFF !important;
+    opacity: 0.9;
+    filter: alpha(opacity=90);
+    font-weight: 600;
+    font-size: 20px;
+    
+    @media (max-width: 767px) {
+        margin: 5px 0px;
+        padding: 20px 15px;
+    }
+`;
+
+NavbarTransparent.Toggle = styled(Navbar.Toggle)`
+    > .icon-bar {
+        background-color: #FFFFFF !important;
+    }
+    margin-top: 19px;
+    margin-bottom: 19px;
+    border: 0;
+
+    @media (max-width: 767px) {
+        margin-top: 17px;
+        margin-bottom: 17px;
+        width: 40px;
+        height: 40px;
+    }
+`;
+
+export default NavbarTransparent;
